2be29e780f8b139526e607d6420bcb520b2e2826
Kastro Kiran V <122141717+KastroVKiran@users.noreply.github.com>
2024-10-11 16:08:19 +0530

    Update HomeController.java

--- src/main/java/com/kastro/spotify/HomeController.java
+++ src/main/java/com/kastro/spotify/HomeController.java
@@ -1,13 +1,14 @@
 package com.kastro.spotify;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";  // This should return the name of your HTML file (without .html)
+    public String home(Model model) {
+        return "index"; // Return the index.html template
     }
 }
